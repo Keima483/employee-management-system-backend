28e6e5cfa14b0e0d37ea25c30c581cf88ebc5ca1
Sahil Verma <vermasahil78910@gmail.com>
2022-11-10 03:28:01 +0530

    init

---
+++ src/main/java/com/keima/employeemanagementsystem/exceptionHandler/ApiExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.keima.employeemanagementsystem.exceptionHandler;
+
+import com.keima.employeemanagementsystem.exceptionHandler.exception.LoginException;
+import com.keima.employeemanagementsystem.model.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = { LoginException.class })
+    public ResponseEntity<ExceptionResponse> handleLoginException(LoginException loginException) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(loginException.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/keima/employeemanagementsystem/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.keima.employeemanagementsystem.repository;
+
+import com.keima.employeemanagementsystem.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
