652ab2a7c62b74b8d3b2ed5f8e2c86ff6b88d8cb
Sahil Verma <vermasahil78910@gmail.com>
2023-05-04 00:23:34 +0530

    added .env

--- src/main/java/com/keima/employeemanagementsystem/api/CompanyRest.java
+++ src/main/java/com/keima/employeemanagementsystem/api/CompanyRest.java
@@ -8,7 +8,14 @@ import com.keima.employeemanagementsystem.model.EmployeeModel;
 import com.keima.employeemanagementsystem.model.LoginDetails;
 import com.keima.employeemanagementsystem.services.CompanyService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- src/main/java/com/keima/employeemanagementsystem/repository/EmployeeRepository.java
+++ src/main/java/com/keima/employeemanagementsystem/repository/EmployeeRepository.java
@@ -3,5 +3,6 @@ package com.keima.employeemanagementsystem.repository;
 import com.keima.employeemanagementsystem.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
